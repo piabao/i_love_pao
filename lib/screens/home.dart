dfe83f20d0cb99b46254ad8f40813ae0c7018586
marlon <eu@marlonh.com>
2018-05-03 17:34:53 -0300

    Changes

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -4,27 +4,47 @@ class home extends StatelessWidget{
 
   String _title = 'Bem Vindo';
 
-  Color _gradientStart = new Color(0xDDA85B11);
-  Color _gradientEnd = Colors.brown[300];
+  Color _gradientStart = new Color(0xFFBC8E65);
+  Color _gradientEnd = new Color(0xFFD7BB96);
+
+  static Color _botaoAcao = new Color(0xFF648CC4);
+
+  static void _onPressed(){
+
+  }
+
+  var comecarPane = new Container(
+    child: new Column(
+      children: <Widget>[
+        new RaisedButton(padding : new EdgeInsets.all(30.0), color : _botaoAcao, child: new Text('Começar'), onPressed: (){_onPressed();}),
+      ],
+    ),
+    margin: const EdgeInsets.all(10.0),
+    padding: const EdgeInsets.all(10.0),
+  );
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       body: new Container(
+        padding: new EdgeInsets.all(32.0),
           child: new Column(
             children: <Widget>[
               new Text(_title, style: new TextStyle(
                   color: Colors.white,
-                  fontSize: 32.0
+                  fontSize: 40.0,
+                  fontFamily: "Lobster",
               ),),
-              new Image.asset('images/info.png')
+              new Image.asset('images/icone1.png', scale: 3.0,),
+              comecarPane,
+              new RaisedButton(child: new Text('Entrar'), onPressed: (){_onPressed();})
             ],
           ),
         constraints: new BoxConstraints.expand(),
         decoration: new BoxDecoration(
           gradient: new LinearGradient(colors: [_gradientStart, _gradientEnd],
-              begin: const FractionalOffset(0.5, 0.0),
-              end: const FractionalOffset(0.0, 0.5),
+              begin: const FractionalOffset(1.0, 0.0),
+              end: const FractionalOffset(0.0, 1.0),
               stops: [0.0,1.0],
               tileMode: TileMode.clamp
           ),
