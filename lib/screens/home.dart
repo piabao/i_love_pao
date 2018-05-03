2014c269d4361310768f199b175226bd9cc7f07d
Marlon <eu@marlonh.com>
2018-05-03 10:15:06 -0300

    Gradient ...

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -4,20 +4,32 @@ class home extends StatelessWidget{
 
   String _title = 'Bem Vindo';
 
+  Color _gradientStart = new Color(0xDDA85B11);
+  Color _gradientEnd = Colors.brown[300];
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       body: new Container(
-        child: new Center(
           child: new Column(
             children: <Widget>[
-              new Text(_title, style: new TextStyle(color: Colors.white, fontSize: 32.0),),
+              new Text(_title, style: new TextStyle(
+                  color: Colors.white,
+                  fontSize: 32.0
+              ),),
               new Image.asset('images/info.png')
             ],
           ),
+        constraints: new BoxConstraints.expand(),
+        decoration: new BoxDecoration(
+          gradient: new LinearGradient(colors: [_gradientStart, _gradientEnd],
+              begin: const FractionalOffset(0.5, 0.0),
+              end: const FractionalOffset(0.0, 0.5),
+              stops: [0.0,1.0],
+              tileMode: TileMode.clamp
+          ),
         ),
-      ),
-      backgroundColor: new Color(0xF1F2D440),
+        ),
     );
   }
 }
